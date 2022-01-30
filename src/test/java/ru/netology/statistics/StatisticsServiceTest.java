cdb4cb55258bb5d1c3b3ddc126e9533d539cdfce
maria-grig <masha.grigorenko.89@gmail.com>
2022-01-30 23:24:07 +0300

    test array corrected

--- src/test/java/ru/netology/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -11,7 +11,7 @@ class StatisticsServiceTest {
 
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
